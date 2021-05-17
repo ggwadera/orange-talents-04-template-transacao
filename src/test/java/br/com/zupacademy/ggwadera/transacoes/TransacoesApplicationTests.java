c43fc285610946ec7c1efecf4e42f2a96bca8e1f
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-05-17 14:54:54 -0300

    setup inicial

---
+++ src/test/java/br/com/zupacademy/ggwadera/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.ggwadera.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

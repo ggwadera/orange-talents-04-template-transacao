cdfeacabb726a7a2d9277ff800b8eb31d798e16a
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-05-18 10:16:59 -0300

    método no repositório para buscar 10 últimas transações pelo id do cartão

--- src/main/java/br/com/zupacademy/ggwadera/transacoes/transacao/TransacaoRepository.java
+++ src/main/java/br/com/zupacademy/ggwadera/transacoes/transacao/TransacaoRepository.java
@@ -2,7 +2,9 @@ package br.com.zupacademy.ggwadera.transacoes.transacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, UUID> {
+    List<Transacao> findFirst10ByCartaoIdOrderByEfetivadaEmDesc(UUID cartaoId);
 }
